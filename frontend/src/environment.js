98381d0b7491cf136f7cf797d3aaa36636982e86
Rahul Verma <rahulverma978404@gmail.com>
2026-07-07 23:40:20 +0530

    Deploying frontend

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,12 +1,10 @@
 
-// let IS_PROD = true;
-// const server = IS_PROD ?
-//     "" :
-//     "http://localhost:8000"
+let IS_PROD = true;
+const server = IS_PROD ?
+    "https://apnavideocallbackend-ighq.onrender.com" :
+    "http://localhost:8000"
 
 
-// export default server;
-
-
-const server = "http://localhost:8000";
 export default server;
+
+
